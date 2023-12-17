26b34b3e7217ca03df540627ff087b7b1652552c
i1ko <oleksiuk13@gmail.com>
2023-12-17 16:43:14 +0200

    fix(color): name renamed to correct value

--- components/info.tsx
+++ components/info.tsx
@@ -11,9 +11,9 @@ interface InfoProps {
 const Info: React.FC<InfoProps> = ({data}) => {
   return (
     <div>
-      <h1 className="text-3xl font-bold text-grey-900">{data.name}</h1>
+      <h1 className="text-3xl font-bold text-gray-900">{data.name}</h1>
       <div className="mt-3 flex items-end justify-between">
-        <p className="text-2xl text-grey-900">
+        <p className="text-2xl text-gray-900">
           <Currency value={data?.price} />
         </p>
       </div>
@@ -27,7 +27,7 @@ const Info: React.FC<InfoProps> = ({data}) => {
         </div>
         <div className="flex items-center gap-x-4">
           <h3 className="font-semibold text-black">Color: </h3>
-          <div className="h-6 w-6 rounded-full border border-grey-600" style={{ backgroundColor: data?.color?.value }} />
+          <div className="h-6 w-6 rounded-full border border-gray-600" style={{ backgroundColor: data?.color?.value }} />
         </div>
       </div>
       <div className="mt-10 flex items-center gap-x-3">
