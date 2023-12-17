1253ba8c6cac40fca670038eac3ab34484505f18
i1ko <oleksiuk13@gmail.com>
2023-12-17 15:34:42 +0200

    feat(gallery): components added

--- app/(routes)/product/[productId]/page.tsx
+++ app/(routes)/product/[productId]/page.tsx
@@ -2,6 +2,7 @@ import getProducts from "@/actions/get-products";
 import getProduct from "@/actions/get-product";
 import Container from "@/components/ui/container";
 import ProductList from "@/components/product-list";
+import Gallery from "@/components/gallery";
 
 interface ProductPageProps {
   params: {
@@ -21,8 +22,8 @@ const ProductPage: React.FC<ProductPageProps> = async ({params}) => {
       <Container>
         <div className="px-4 py-10 sm:px-6 lg:px-8">
           <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
-          {/*  Gallery */}
-            <div>Gallery</div>
+            {/*  Gallery */}
+            <Gallery images={product.images} />
             <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
             {/*  Info */}
               Info
