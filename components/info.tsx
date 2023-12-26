c44a74947513695c852c5d7a5d9fb2488d3cc9ec
i1ko <oleksiuk13@gmail.com>
2023-12-26 21:23:45 +0200

    fix(add-to-cart): in info.tsx button was without an onclick handler. Fixed

--- components/info.tsx
+++ components/info.tsx
@@ -1,14 +1,23 @@
-import React from 'react';
+"use client";
+
+import React, {MouseEventHandler} from 'react';
 import {ShoppingCart} from "lucide-react";
 import Currency from "@/components/ui/currency";
 import Button from "@/components/ui/button";
 import {Product} from "@/types";
+import useCart from "@/hooks/use-cart";
 
 interface InfoProps {
   data: Product;
 }
 
 const Info: React.FC<InfoProps> = ({data}) => {
+  const cart = useCart();
+  const onAddToCart: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.stopPropagation();
+    cart.addItem(data);
+  }
+
   return (
     <div>
       <h1 className="text-3xl font-bold text-gray-900">{data.name}</h1>
@@ -20,6 +29,7 @@ const Info: React.FC<InfoProps> = ({data}) => {
       <hr className="my-4" />
       <div className="flex flex-col gap-y-6">
         <div className="flex items-center gap-x-4">
+          {/* todo: localization */}
           <h3 className="font-semibold text-black">Size: </h3>
           <div>
             {data?.size?.name}
@@ -31,7 +41,9 @@ const Info: React.FC<InfoProps> = ({data}) => {
         </div>
       </div>
       <div className="mt-10 flex items-center gap-x-3">
-        <Button className="flex items-center gap-x-2">
+        <Button
+          onClick={onAddToCart}
+          className="flex items-center gap-x-2">
           Add To Cart
           <ShoppingCart />
         </Button>
